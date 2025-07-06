a3e8913589fea3aab24d3473eac607f290580d1a
kartik1502 <kartikkulkarni1411@gmail.com>
2025-07-06 15:02:27 +0530

    initial commit

---
+++ src/main/java/org/arya/banking/common/AryaBankingCommonApplication.java
@@ -0,0 +1,13 @@
+package org.arya.banking.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AryaBankingCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AryaBankingCommonApplication.class, args);
+    }
+
+}
