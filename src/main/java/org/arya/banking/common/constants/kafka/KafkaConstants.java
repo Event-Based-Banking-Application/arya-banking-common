2cf43aa8325a416e96c64823e6e0c376ec9035da
Karthik Kulkarni <64131481+kartik1502@users.noreply.github.com>
2025-10-18 19:27:40 +0530

    Feature/#94 complete registration flow (#7)
    * migrate: commit due to moving to linux
    
    Refs: #94
    
    * feat: update address, constants, exceptions, avro and configs.
    
    Refs: #94
    
    ---------
    
    Co-authored-by: Kishan1502 <kulkarnikishan1502@gmail.com>

--- src/main/java/org/arya/banking/common/constants/kafka/KafkaConstants.java
+++ src/main/java/org/arya/banking/common/constants/kafka/KafkaConstants.java
@@ -11,4 +11,8 @@ public interface KafkaConstants {
     String STRING_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
     String KAFKA_AVRO_DESERIALIZER = "io.confluent.kafka.serializers.KafkaAvroDeserializer";
 
+    //kafka topic
+    String USER_CREATE_EVENT = "user.create.event";
+    String AUDIT_EVENT = "audit.event";
+
 }
