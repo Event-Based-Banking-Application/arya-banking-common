55d13a363ee067a28c2d87366d2a020927e3734d
Karthik Kulkarni <64131481+kartik1502@users.noreply.github.com>
2025-10-25 13:39:46 +0530

    Feature/#16 lock account (#8)
    * feat: refactor the response, exception code and add UserResponse dto.
    
    Refs: #16
    
    * feat: add exception codes and response keys.
    
    Refs: #16

--- src/main/java/org/arya/banking/common/constants/ResponseCodes.java
+++ src/main/java/org/arya/banking/common/constants/ResponseCodes.java
@@ -2,6 +2,7 @@ package org.arya.banking.common.constants;
 
 public interface ResponseCodes {
 
-    String USER_CREATED_CODE = "USER_USECR_201";
-    String USER_UPDATED_CODE = "USER_USRUP_201";
+    String USER_CREATED_201 = "USER_CREATED_201";
+    String USER_UPDATED_200 = "USER_UPDATED_200";
+    String SECURITY_DETAILS_UPDATED_200 = "SECURITY_DETAILS_UPDATED_200";
 }
---
+++ src/main/java/org/arya/banking/common/constants/ResponseKeys.java
@@ -0,0 +1,9 @@
+package org.arya.banking.common.constants;
+
+public interface ResponseKeys {
+
+    String USER_ID = "userId";
+    String RESPONSE = "response";
+    String RESPONSE_CODE = "responseCode";
+    String DISABLE_USER = "disableUser";
+}
