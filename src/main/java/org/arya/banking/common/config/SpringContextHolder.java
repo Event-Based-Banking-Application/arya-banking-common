d571864701358925bffc8197926f4e34683629a0
Karthik Kulkarni <64131481+kartik1502@users.noreply.github.com>
2026-01-24 22:22:49 +0530

    feat: add custom exceptions for Vault operations and utility methods for configuration loading (#10)

---
+++ src/main/java/org/arya/banking/common/config/SpringContextHolder.java
@@ -0,0 +1,26 @@
+package org.arya.banking.common.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextHolder implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+
+    public static Resource getResource(String path) {
+
+        if (applicationContext != null) {
+            return applicationContext.getResource(path);
+        }
+        return null;
+    }
+}
